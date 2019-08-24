96c775b49c1b8512d257875342c3da587b0032ab
jover <jover@mit.edu>
2019-08-24 11:17:01 -0400

    Home page navigation

--- js/main.js
+++ js/main.js
@@ -30,11 +30,5 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
 }
-$(document).ready(function() {
-    $('.hover').on('touchstart touchend', function(e) {
-        e.preventDefault();
-        $(this).toggleClass('hover_effect');
-    });
-})
 );
 
